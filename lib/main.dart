92a93946c997dfe4aa2ac95d0fda2b992cceb3fc
Eric Mengelkoch <134116393+EricMengelkoch@users.noreply.github.com>
2024-08-01 14:25:21 -0500

    added new pages and html requests

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:weatherapp/pages/choose_location.dart';
+import 'package:weatherapp/pages/loading.dart';
+import 'pages/home.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: 
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
   ));
 }
 
